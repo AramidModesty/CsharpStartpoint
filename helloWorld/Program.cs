0a31a25047ba2233046100749a07d76f1897eda6
Aramid Mauricio Monsalve Usuga <aramidps4@gmail.com>
2026-02-27 15:38:44 -0500

    Hello world

--- helloWorld/Program.cs
+++ helloWorld/Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Programa funcionando\nPresione una tecla "+
+"para continuar");
+Console.ReadLine();
