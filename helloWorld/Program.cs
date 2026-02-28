4051824e9e80ac2ea2d515919e555e6e453aaffa
Aramid Mauricio Monsalve Usuga <aramidps4@gmail.com>
2026-02-28 09:23:02 -0500

    Improv

--- helloWorld/Program.cs
+++ helloWorld/Program.cs
@@ -2,3 +2,13 @@
 Console.WriteLine("Programa funcionando\nPresione una tecla "+
 "para continuar");
 Console.ReadLine();
+namespace HelloWorld
+{
+    class program(){
+        static void Main()
+        {
+            var name="Whatev";
+            Console.WriteLine($"Hello {name}");   
+        }
+    }
+}
